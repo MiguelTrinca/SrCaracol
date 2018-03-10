78db86b1628290caf947f04a8fe432651259803a
guestDD <SRID03WMhw@protonmail.com>
2018-03-10 11:59:06 +0000

    added main file

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+  file: main.cpp
+*/
+
+# include <stdio.h>
+
+void parse_inputs_or_exit() {
+	int nodes_num;
+	int edges_num;
+
+	sscanf(str, "%d", &nodes_num);
+	sscanf(str, "%d", &edges_num);
+}
+
+int main(int argc, char const *argv[]) {
+
+	return 0;
+}
